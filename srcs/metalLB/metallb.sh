24958214aa7d34816d33ebc3e477370977fac0cc
Thomas Berthet <thoberth@student.42.fr>
2021-04-29 18:13:57 +0200

    1212

---
+++ srcs/metalLB/metallb.sh
@@ -0,0 +1,7 @@
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metalLB/metallbconfig.yaml
